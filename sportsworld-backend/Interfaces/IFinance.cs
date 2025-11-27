5a3bf48538936e741f725fee377161bc68b6fa40
mariusaellingsen <marius.a.ellingsen@gmail.com>
2025-11-27 13:58:49 +0100

    set up database in backend

---
+++ sportsworld-backend/Interfaces/IFinance.cs
@@ -0,0 +1,9 @@
+namespace sportsworld_backend.Interfaces;
+
+interface IFinance
+{
+    int Id { get; set; }
+    int MoneyLeft { get; set; }
+    int NumberOfPurchases { get; set; }
+    int MoneySpent { get; set; }
+}
--- sportsworld-backend/Program.cs
+++ sportsworld-backend/Program.cs
@@ -1,12 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using sportsworld_backend.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<SportsWorldContext>(
+    options => options.UseSqlite("Data source = Databases/SportsWorldData.db")
+);
+
+builder.Services.AddCors(
+    options =>
+    {
+        options.AddPolicy("AllowAnyOrigin",
+            policies => policies
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                );
+    }
+);
+
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.UseCors("AllowAnyOrigin");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
