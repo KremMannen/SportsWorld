490f4638a3f14416b9e2b7b47b2e853616631c48
KremMannen <flyingnorwegians@gmail.com>
2025-11-28 22:42:38 +0100

    added basic crud to the other controllers

--- sportsworld-backend/Controllers/FinanceController.cs
+++ sportsworld-backend/Controllers/FinanceController.cs
@@ -8,19 +8,90 @@ namespace sportsworld_backend.Controllers;
 [ApiController]
 [Route("api/[controller]")]
 
-public class FinanceController (SportsWorldContext _SportsWorldContext) : ControllerBase
+public class FinanceController (SportsWorldContext _context) : ControllerBase
 {
     [HttpGet]
-        public async Task<ActionResult<List<Finance>>> Get()
+    public async Task<ActionResult<List<Finance>>> Get()
     {
         try
         {
-            List<Finance> finances = await _SportsWorldContext.Finances.ToListAsync();
+            List<Finance> finances = await _context.Finances.ToListAsync();
             return Ok(finances);
         }
         catch (Exception e)
         {
             return StatusCode(500, $"En feil oppsto: {e.Message}");
+        }
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Finance>> Get(int id)
+    {
+        try
+        {
+            Finance? finance = await _context.Finances.FindAsync(id);
+            if (finance == null)
+            {
+                return NotFound("No finance found with that ID.");
+            }
+            return Ok(finance);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, $"Error: {e.Message}");
+        }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Finance>> Post(Finance newFinance)
+    {
+        try
+        {
+            _context.Finances.Add(newFinance);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("Get", new { id = newFinance.Id }, newFinance);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, $"Error: {e.Message}");
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        try
+        {
+            Finance? finance = await _context.Finances.FindAsync(id);
+            if (finance != null)
+            {
+                _context.Finances.Remove(finance);
+                await _context.SaveChangesAsync();
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, $"Error: {e.Message}");
+        }
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<Finance>> Put(Finance editedFinance)
+    {
+        try
+        {
+            _context.Entry(editedFinance).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, $"Error: {e.Message}");
         }
     }
 }
--- sportsworld-backend/Controllers/VenueController.cs
+++ sportsworld-backend/Controllers/VenueController.cs
@@ -8,19 +8,90 @@ namespace sportsworld_backend.Controllers;
 [ApiController]
 [Route("api/[controller]")]
 
-public class VenueController (SportsWorldContext _SportsWorldContext) : ControllerBase
+public class VenueController (SportsWorldContext _context) : ControllerBase
 {
     [HttpGet]
-        public async Task<ActionResult<List<Venue>>> Get()
+    public async Task<ActionResult<List<Venue>>> Get()
     {
         try
         {
-            List<Venue> venues = await _SportsWorldContext.Venues.ToListAsync();
+            List<Venue> venues = await _context.Venues.ToListAsync();
             return Ok(venues);
         }
         catch (Exception e)
         {
             return StatusCode(500, $"En feil oppsto: {e.Message}");
+        }
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Venue>> Get(int id)
+    {
+        try
+        {
+            Venue? venue = await _context.Venues.FindAsync(id);
+            if (venue == null)
+            {
+                return NotFound("No venue found with that ID.");
+            }
+            return Ok(venue);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, $"Error: {e.Message}");
+        }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Venue>> Post(Venue newVenue)
+    {
+        try
+        {
+            _context.Venues.Add(newVenue);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("Get", new { id = newVenue.Id }, newVenue);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, $"Error: {e.Message}");
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        try
+        {
+            Venue? venue = await _context.Venues.FindAsync(id);
+            if (venue != null)
+            {
+                _context.Venues.Remove(venue);
+                await _context.SaveChangesAsync();
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, $"Error: {e.Message}");
+        }
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<Venue>> Put(Venue editedVenue)
+    {
+        try
+        {
+            _context.Entry(editedVenue).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, $"Error: {e.Message}");
         }
     }
 }
