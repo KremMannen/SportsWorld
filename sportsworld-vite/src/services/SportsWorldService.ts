734112cd31f07df1442d1789968204b3caf9bf21
KremMannen <flyingnorwegians@gmail.com>
2025-12-01 18:20:00 +0100

    completed service functions for finances and venues

--- sportsworld-vite/src/services/SportsWorldService.ts
+++ sportsworld-vite/src/services/SportsWorldService.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 import type { IAthlete } from "../interfaces/IAthlete";
+import type { IVenue } from "../interfaces/IVenue";
+import type { IFinance } from "../interfaces/IFinance";
 
 const athleteEndpoint = "http://localhost:5003/api/athlete";
 const venueEndpoint = "http://localhost:5003/api/venue";
 const financeEndpoint = "http://localhost:5003/api/finance";
 
-// Fremfor å returnere null / [] i disse funksjonene throwes erroren
+// Fremfor å returnere null / [] i catchen til disse funksjonene throwes erroren
 // Dette for å kunne bruke de til å gi brukeren feedback på UI nivå
 
 const getAthletes = async (): Promise<IAthlete[]> => {
@@ -17,7 +19,6 @@ const getAthletes = async (): Promise<IAthlete[]> => {
     throw error;
   }
 };
-
 const getAthleteById = async (id: number): Promise<IAthlete> => {
   try {
     const { data } = await axios.get<IAthlete>(`${athleteEndpoint}/${id}`);
@@ -27,7 +28,6 @@ const getAthleteById = async (id: number): Promise<IAthlete> => {
     throw error;
   }
 };
-
 const getAthleteByName = async (query: string): Promise<IAthlete> => {
   try {
     const result = await axios.get<IAthlete>(
@@ -39,7 +39,6 @@ const getAthleteByName = async (query: string): Promise<IAthlete> => {
     throw error;
   }
 };
-
 const postAthlete = async (newAthlete: IAthlete): Promise<IAthlete> => {
   try {
     const { data } = await axios.post<IAthlete>(athleteEndpoint, newAthlete);
@@ -49,7 +48,6 @@ const postAthlete = async (newAthlete: IAthlete): Promise<IAthlete> => {
     throw error;
   }
 };
-
 const deleteAthlete = async (id: number): Promise<void> => {
   try {
     await axios.delete(`${athleteEndpoint}/${id}`);
@@ -58,7 +56,6 @@ const deleteAthlete = async (id: number): Promise<void> => {
     throw error;
   }
 };
-
 const putAthlete = async (editedAthlete: IAthlete): Promise<void> => {
   try {
     await axios.put(athleteEndpoint, editedAthlete);
@@ -67,3 +64,78 @@ const putAthlete = async (editedAthlete: IAthlete): Promise<void> => {
     throw error;
   }
 };
+const getVenues = async (): Promise<IVenue[]> => {
+  try {
+    const { data } = await axios.get<IVenue[]>(venueEndpoint);
+    return data;
+  } catch (error) {
+    console.error("getVenues: Failed to fetch venues:", error);
+    throw error;
+  }
+};
+const getVenueById = async (id: number): Promise<IVenue> => {
+  try {
+    const { data } = await axios.get<IVenue>(`${venueEndpoint}/${id}`);
+    return data;
+  } catch (error) {
+    console.error("getVenueById: Failed to fetch venue:", error);
+    throw error;
+  }
+};
+const getVenueByName = async (query: string): Promise<IVenue> => {
+  try {
+    const result = await axios.get<IVenue>(`${venueEndpoint}/byname/${query}`);
+    return result.data;
+  } catch (error) {
+    console.error("getVenueByName: Failed to fetch venues:", error);
+    throw error;
+  }
+};
+const postVenue = async (newVenue: IVenue): Promise<IVenue> => {
+  try {
+    const { data } = await axios.post<IVenue>(venueEndpoint, newVenue);
+    return data;
+  } catch (error) {
+    console.error("postVenue: Failed to post venue:", error);
+    throw error;
+  }
+};
+const deleteVenue = async (id: number): Promise<void> => {
+  try {
+    await axios.delete(`${venueEndpoint}/${id}`);
+  } catch (error) {
+    console.error("deleteAthlete: Failed to delete venue:", error);
+    throw error;
+  }
+};
+const putVenue = async (editedVenue: IVenue): Promise<void> => {
+  try {
+    await axios.put(venueEndpoint, editedVenue);
+  } catch (error) {
+    console.error("putVenue: Failed to put venue:", error);
+    throw error;
+  }
+};
+// Her er det bare 1 objekt som returneres,
+// men vi vedlikeholder mønsteret til get funksjoner og returnerer liste
+const getFinances = async (): Promise<IFinance[]> => {
+  try {
+    const { data } = await axios.get<IFinance[]>(financeEndpoint);
+    return data;
+  } catch (error) {
+    console.error("getFinances: Failed to fetch finances:", error);
+    throw error;
+  }
+};
+
+const putFinance = async (editedFinance: IFinance): Promise<void> => {
+  try {
+    await axios.put(financeEndpoint, editedFinance);
+  } catch (error) {
+    console.error("putFinance: Failed to put finance:", error);
+    throw error;
+  }
+};
+
+// Not implemented
+// POST and Delete -- Not necessary in current scope
