d471405effed95b694321d2242d683ef7351b7ff
KremMannen <flyingnorwegians@gmail.com>
2025-12-01 20:52:22 +0100

    fixed imageuploadservice

---
+++ sportsworld-vite/src/services/ImageUploadService.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+import type { IUploadResponse } from "../interfaces/IUploadResponse";
+
+const venueEndpoint = "http://localhost:5110/api/ImageUpload/venue";
+const athleteEndpoint = "http://localhost:5110/api/ImageUpload/athlete";
+
+const uploadVenueImage = async (image: File): Promise<IUploadResponse> => {
+  return uploadImage(image, venueEndpoint);
+};
+
+const uploadAthleteImage = async (image: File): Promise<IUploadResponse> => {
+  return uploadImage(image, athleteEndpoint);
+};
+
+// API svarer med filnavnet, som brukes til å generere filsti for visning
+// av bildet senere
+const uploadImage = async (
+  image: File,
+  endpoint: string
+): Promise<IUploadResponse> => {
+  const formData = new FormData();
+  formData.append("file", image);
+
+  try {
+    const { data } = await axios({
+      url: endpoint,
+      method: "POST",
+      data: formData,
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+    return data;
+  } catch (error) {
+    console.error("Image upload failed:", error);
+    alert("Failed to upload image. Please try again.");
+    throw error;
+  }
+};
+
+export default { uploadVenueImage, uploadAthleteImage };
