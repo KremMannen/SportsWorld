34aa265d7fdc88e327ae7a6290dde2270de83ff7
KremMannen <flyingnorwegians@gmail.com>
2025-12-04 14:47:29 +0100

    venues list polish

--- sportsworld-vite/src/components/FeaturedVenues.tsx
+++ sportsworld-vite/src/components/FeaturedVenues.tsx
@@ -11,12 +11,12 @@ export const FeaturedVenues: FC<IFeaturedVenuesProps> = () => {
 
   return (
     <div className="container px-4 py-8 max-w-screen-2xl mx-auto">
-      <h2 className="text-3xl font-bold mb-6">Featured Fighters</h2>
+      <h2 className="text-3xl font-bold mb-6">Featured venues</h2>
 
       {/* Innhold laster inn */}
       {isLoading ? (
         <div className="flex justify-center items-center py-12">
-          <div className="text-gray-500 text-lg">Loading fighters...</div>
+          <div className="text-gray-500 text-lg">Loading venues...</div>
         </div>
       ) : errorMessage || venues.length === 0 ? (
         /* Feilmelding. Om noe gikk galt vises errorMessage fra context, om ikke informeres det bare om at det ikke er noen venues tilgjengelig (tom database feks)*/
