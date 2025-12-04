52f8013ecc1c48234b2495c3188d31829457452e
mariusaellingsen <marius.a.ellingsen@gmail.com>
2025-12-04 13:11:34 +0100

    centered featured fighters on main page and added max width

--- sportsworld-vite/src/interfaces/properties/IFeaturedFightersProps.tsx
+++ sportsworld-vite/src/interfaces/properties/IFeaturedFightersProps.tsx
@@ -1,4 +1,3 @@
-import type { IAthlete } from "../IAthlete";
 
 export interface IFeaturedFightersProps {
   // I Figma vises listen med side-scroll fremfor tile-layout. Introdusere variant property?
