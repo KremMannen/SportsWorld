e1f66c44c0837c5e3a42ead0a549519cbf33d271
KremMannen <flyingnorwegians@gmail.com>
2025-12-05 12:19:54 +0100

    added layou comment

--- sportsworld-vite/src/components/PageLayout.tsx
+++ sportsworld-vite/src/components/PageLayout.tsx
@@ -1,6 +1,8 @@
 import type { FC } from "react";
 import type { IPageLayoutProps } from "../interfaces/properties/IPageLayoutProps";
 
+// En enkel layout-komponent som sentrerer definering av padding og maks-bredde for innholdet.
+
 export const PageLayout: FC<IPageLayoutProps> = ({ children }) => {
   return (
     <main className="container max-w-[1600px] mx-auto p-12">{children}</main>
