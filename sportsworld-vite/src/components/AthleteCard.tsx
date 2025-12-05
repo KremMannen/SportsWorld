be9dae7cc3eb1292db3ad51350759da949829033
mariusaellingsen <marius.a.ellingsen@gmail.com>
2025-12-05 12:58:03 +0100

    fixed link click on view-cards

--- sportsworld-vite/src/components/AthleteCard.tsx
+++ sportsworld-vite/src/components/AthleteCard.tsx
@@ -1,5 +1,7 @@
 import type { FC } from "react";
 import type { IAthleteCardProps } from "../interfaces/properties/IAthleteCardProps.ts";
+import { Link } from "react-router-dom";
+
 
 // Vi ønsker en modulær AthleteCard komponent som kan generere de forskjellige variantene vi
 // skisserte i prototype-fasen.
@@ -27,6 +29,9 @@ export const AthleteCard: FC<IAthleteCardProps> = ({
   const cardHoverEffect = variant === "manage" || variant === "sign" ? "hover:shadow-black/40" : "hover:scale-[1.05] hover:shadow-black/40 hover:cursor-pointer" ;
   const buttonBase = "px-4 py-2 rounded transition-transform transition-shadow duration-200 transform text-white ";
   const buttonHover = "hover:shadow hover:cursor-pointer hover:border border-red-600";
+
+  // Linker kort til aktuelle pages. Your fighters sender deg til admin og available fighters sender deg til finances for å kjøpe
+  const viewCardHref = athlete.purchased ? "/admin" : "/finances";
 
   const renderButtons = () => {
     switch (variant) {
@@ -72,9 +77,10 @@ export const AthleteCard: FC<IAthleteCardProps> = ({
     }
   };
 
-  return (
-    <article className={`${bgColor} col-span-12 sm:col-span-6 lg:flex-shrink-0 lg:w-[400px] xl:col-span-3 xl:w-auto rounded-lg shadow-md shadow-black/20 flex overflow-hidden ${cardHeight}
-    transition-transform duration-200 ${cardHoverEffect} `}>
+  const articleJSX = (
+    <article
+      className={`${bgColor} rounded-lg shadow-md shadow-black/20 flex overflow-hidden ${cardHeight} w-full`}
+    >
       <div className={`${imageSize} flex-shrink-0`}>
         <img
           src={`http://localhost:5110/images/AthleteImages/${athlete.image}`}
@@ -93,5 +99,20 @@ export const AthleteCard: FC<IAthleteCardProps> = ({
         <div className="flex gap-2">{renderButtons()}</div>
       </div>
     </article>
+  );
+
+  return variant === "view" ? (
+    <Link
+      to={viewCardHref}
+      className={`block col-span-12 sm:col-span-6 lg:flex-shrink-0 lg:w-[400px] xl:col-span-3 xl:w-auto transition-transform duration-200 ${cardHoverEffect}`}
+    >
+      {articleJSX}
+    </Link>
+  ) : (
+    <div
+      className={`col-span-12 sm:col-span-6 lg:flex-shrink-0 lg:w-[400px] xl:col-span-3 xl:w-auto transition-transform duration-200 ${cardHoverEffect}`}
+    >
+      {articleJSX}
+    </div>
   );
 };
