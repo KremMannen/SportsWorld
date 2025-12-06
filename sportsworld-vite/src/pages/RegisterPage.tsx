181a8eebda8b3b6fda7d6556d45ee84d99ee9aaa
KremMannen <flyingnorwegians@gmail.com>
2025-12-06 19:47:49 +0100

    .

--- sportsworld-vite/src/pages/RegisterPage.tsx
+++ sportsworld-vite/src/pages/RegisterPage.tsx
@@ -1,9 +1,11 @@
 import { AthleteList } from "../components/athlete/AthleteList";
+import { AthleteRegister } from "../components/athlete/AthleteRegister";
 import { PageLayout } from "../components/common/PageLayout";
 
 const RegisterPage = () => {
   return (
     <PageLayout>
+      <AthleteRegister />
       <AthleteList filterType="all" cardVariant="manage" />
     </PageLayout>
   );
