c3ce3e8c14e7041d750ea003377c2a1cbe0a6ebe
KremMannen <flyingnorwegians@gmail.com>
2025-12-06 23:44:25 +0100

    added comments 2

--- sportsworld-vite/src/interfaces/objects/IAthlete.ts
+++ sportsworld-vite/src/interfaces/objects/IAthlete.ts
@@ -1,10 +1,6 @@
 // ID og image trenger ikke være nullable. Vi benytter Omit<IAthlete, "id"> i post funksjonen, og Omit<IAthlete, "id | image" i context-nivå funksjonen>.
-
-// Grunn:
 // Reflekterer at enhver atlet fra databasen har Id og image.
 // På komponent nivå tillater det f.eks å bruke athlete.id, uten at man trenger å kontrollere om den finnes eller bruke non-null assertion.
-// I praksis når man genererer komponenter på page-nivå vil enhver atlet som bearbeides ha en id og image.
-// Nå reflekterer koden også det.
 
 export interface IAthlete {
   id: number;
