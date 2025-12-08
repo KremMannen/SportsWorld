3b825d80a8d1bdf9179d72c28109c624a341fcf8
mariusaellingsen <marius.a.ellingsen@gmail.com>
2025-12-08 13:12:10 +0100

    fixed header styling with conditional styling, used new react hook useLocation

--- sportsworld-vite/src/components/common/Header.tsx
+++ sportsworld-vite/src/components/common/Header.tsx
@@ -1,26 +1,45 @@
 import type { FC } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header: FC = () => {
 
-  const hoverEffect ="hover:text-red-700"
-  const listItemStyling = `text-white text-2xl hover:text-red-300 font-medium flex items-center ${hoverEffect}`
+  // bruker useLocation hook for å hente brukerens nåværende sted i nettsiden.
+  const { pathname } = useLocation();
+
+  // Alle nav-elementer i listen har en boks, som endrer styling dersom man er på siden elementet linker til.
+const getNavigationItemStyling = (route: string) => {
+
+  const baseStyling= `
+    bg-[#4C0000]
+    text-white text-sm lg:text-xl font-medium
+    h-10
+    px-2 lg:px-4 py-2
+    rounded-md
+    hover:bg-[#870000]
+    flex items-center gap-2
+    whitespace-nowrap
+`;
+  // Boksen endrer bakgrunn og får rød border dersom route matcher url-objektet lagret i useLocation
+  const activeClasses = pathname === route ? "bg-[#870000] border-2 border-red-600 shadow-sm" : "";
+
+  return `${baseStyling} ${activeClasses}`;
+}
 
   return (
     <header className="bg-black shadow-md px-4 py-2">
       <nav className="flex justify-between px-4">
         <Link
           to="/"
-          className= {`text-white text-4xl font-bold ${hoverEffect}`}
+          className= "text-white text-4xl font-bold hover:text-red-600"
         >
           SportsWorld
         </Link>
 
-         <ul className="flex items-center space-x-6">
+         <ul className="flex items-center gap-2 lg:space-x-6 overflow-x-auto flex-nowrap">
           <li>
             <Link
               to="/admin"
-              className= {`${listItemStyling}`}
+              className= {getNavigationItemStyling("/admin")}
             >
               {/* Viser ikon på små skjermer */}
               {/* Må bruke !inline osv for å force endringene i tailwind. Hvis ikke forsvinner ikke ikonene, vises heller sammen med ordet. */}
@@ -33,7 +52,7 @@ const Header: FC = () => {
           <li>
             <Link
               to="/register"
-              className= {`${listItemStyling}`}
+              className= {getNavigationItemStyling("/register")}
             >
               <i className="fas fa-user-plus !inline lg:!hidden"></i>
               <span className="!hidden lg:!inline">Register</span>
@@ -43,7 +62,7 @@ const Header: FC = () => {
           <li>
             <Link
               to="/finances"
-              className= {`${listItemStyling}`}
+              className= {getNavigationItemStyling("/finances")}
             >
               <i className="fas fa-coins !inline lg:!hidden"></i>
               <span className="!hidden lg:!inline">Finances</span>
@@ -53,7 +72,7 @@ const Header: FC = () => {
           <li>
             <Link
               to="/venues"
-              className= {`${listItemStyling}`}
+              className= {getNavigationItemStyling("/venues")}
             >
               <i className="fas fa-building !inline lg:!hidden"></i>
               <span className="!hidden lg:!inline">Venues</span>
