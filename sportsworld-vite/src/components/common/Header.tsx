7b554cc989f49282f1252c5fc2f82b37e7b35ddb
mariusaellingsen <marius.a.ellingsen@gmail.com>
2025-12-08 14:13:23 +0100

    fixed header sizing

--- sportsworld-vite/src/components/common/Header.tsx
+++ sportsworld-vite/src/components/common/Header.tsx
@@ -1,65 +1,63 @@
 import type { FC } from "react";
 import { NavLink } from "react-router-dom";
 
-// Vi velger å bruke NavLink for å enkelt kunne bruke isActive propertien til å kontrollere stylingen av hvert element i listen basert på hvilken page man bruker er på.
-// Alternativt så kunne vi brukt useLocation-hooken for å sammenligne route med url-objektet useLocation lagrer, og style basert på om route og url-path matcher, men da dette er utenfor pensum valgte vi en enkel NavLink løsning
-
 const Header: FC = () => {
   const baseStyling =
-    "bg-[#4C0000] text-white text-sm lg:text-xl font-medium h-10 px-2 lg:px-4 py-2 rounded-md hover:bg-[#870000] flex items-center gap-2 whitespace-nowrap";
-
+    "h-10 w-10 bg-[#4C0000] text-white text-xs sm:text-base md:text-md lg:text-lg xl:text-xl font-medium h-8 sm:h-10 px-2 sm:px-3 lg:px-4 py-1 sm:py-2 rounded-md hover:bg-[#870000] flex items-center gap-2 whitespace-nowrap";
   const activeStyling = "bg-[#870000] border-1 border-red-600 shadow-sm";
+  const listItemMargin = "mr-2 sm:mr-2 lg:mr-4";
+  const iconStyling = "!inline sm:!hidden fas text-xl ";
+  const spanStyling = "!hidden sm:!inline";
 
   return (
-    <header className="bg-black shadow-md px-4 py-2">
+    <header className="bg-black shadow-md px-4 py-4">
       <nav className="flex justify-between px-4">
-        <NavLink to="/" className="text-white text-4xl font-bold hover:text-red-600">
+        <NavLink to="/" className="text-white text-3xl lg:text-4xl font-bold hover:text-red-600">
           SportsWorld
         </NavLink>
 
-        <ul className="flex items-center gap-2 lg:space-x-6 overflow-x-auto flex-nowrap">
-
-          <li>
+        <ul className="flex items-center overflow-x-auto flex-nowrap">
+          <li className={listItemMargin}>
             <NavLink
               to="/admin"
               className={({ isActive }) => 
                 (isActive ? `${baseStyling} ${activeStyling}` : baseStyling)}
             >
-              <i className="fas fa-user-shield !inline lg:!hidden"></i>
-              <span className="!hidden lg:!inline">Admin</span>
+              <i className={`fa-user-shield ${iconStyling}`}></i>
+              <span className={spanStyling}>Admin</span>
             </NavLink>
           </li>
 
-          <li>
+          <li className={listItemMargin}>
             <NavLink
               to="/register"
               className={({ isActive }) => 
                 (isActive ? `${baseStyling} ${activeStyling}` : baseStyling)}
             >
-              <i className="fas fa-user-plus !inline lg:!hidden"></i>
-              <span className="!hidden lg:!inline">Register</span>
+              <i className={`fa-user-plus ${iconStyling}`}></i>
+              <span className={spanStyling}>Register</span>
             </NavLink>
           </li>
 
-          <li>
+          <li className={listItemMargin}>
             <NavLink
               to="/finances"
               className={({ isActive }) => 
                 (isActive ? `${baseStyling} ${activeStyling}` : baseStyling)}
             >
-              <i className="fas fa-coins !inline lg:!hidden"></i>
-              <span className="!hidden lg:!inline">Finances</span>
+              <i className={`fa-coins ${iconStyling}`}></i>
+              <span className={spanStyling}>Finances</span>
             </NavLink>
           </li>
 
-          <li>
+          <li className={listItemMargin}>
             <NavLink
               to="/venues"
               className={({ isActive }) => 
                 (isActive ? `${baseStyling} ${activeStyling}` : baseStyling)}
             >
-              <i className="fas fa-building !inline lg:!hidden"></i>
-              <span className="!hidden lg:!inline">Venues</span>
+              <i className={`fa-building ${iconStyling}`}></i>
+              <span className={spanStyling}>Venues</span>
             </NavLink>
           </li>
 
